84a370bde3b1f7380636f55ec8389282f2d0ee27
Archana-in-git <archanavs2911@gmail.com>
2025-05-30 19:33:59 +0530

    changes made

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
-import HomeIcon from '@mui/icons-material/Home';
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function ButtonAppBar() {
   return (
@@ -24,43 +24,41 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
 
-           <Link to="/">
-          <IconButton
-            size="large"
-            edge="start"
-            sx={{ mr: 2 }}
-            color="inherit"
-            
-          >
-            <HomeIcon/>
-          </IconButton></Link>
-          
+          <Link to="/">
+            <IconButton
+              size="large"
+              edge="start"
+              sx={{ mr: 2 }}
+              color="inherit"
+            >
+              <HomeIcon />
+            </IconButton>
+          </Link>
 
           <Link to="/Register">
-            <Button  color="inherit">Register</Button>
+            <Button color="inherit">Register</Button>
           </Link>
           <Link to="/Login">
-            <Button  color="inherit">Login</Button>
+            <Button color="inherit">Login</Button>
           </Link>
           <Link to="/NotFOund">
             <Button color="inherit">NotFound</Button>
           </Link>
-          
-            <Link to="/Cart">
+          <Link to="/Checkout">
+            <Button color="inherit">Checkout</Button>
+          </Link>
+
+          <Link to="/Cart">
             <IconButton
               size="large"
               edge="end"
               aria-label="account of current user"
-              
               aria-haspopup="true"
-              
               color="inherit"
             >
               <ShoppingCartIcon />
-            </IconButton></Link>
-
-
-
+            </IconButton>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,17 +1,15 @@
-// src/services/productService.js
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api/products";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 // Get all products
 export const getAllProducts = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(`${BASE_URL}/products`);
   return response.data;
 };
 
 // Get single product by ID
 export const getProductById = async (id) => {
-  const response = await axios.get(`${API_URL}/${id}`);
+  const response = await axios.get(`${BASE_URL}/products/${id}`);
   return response.data;
 };
