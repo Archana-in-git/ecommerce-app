4982acb40a1f3ec49e30b1fdee0a442b6dea2607
Archana-in-git <archanavs2911@gmail.com>
2025-06-04 21:27:39 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("API is running");
 });
